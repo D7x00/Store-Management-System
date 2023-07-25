2692f5aa3fb8503fdeaae58c3fd66c51699c7661
Kareem_DC7X <kareem741ahmed@gmail.com>
2023-07-25 04:25:13 +0300

    First App

---
+++ DBStoreMangment/DBModel/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBModel.Models
+{
+    public class Department
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+        [Required, MaxLength(250)]
+        public string Name { get; set; }
+    }
+}
---
+++ DBStoreMangment/DBModel/Models/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBModel.Models
+{
+    public class StoreContext:DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.
+           UseSqlServer("Data Source =.; Initial Catalog = StoreManagement; Integrated Security = True; TrustServerCertificate = True");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
