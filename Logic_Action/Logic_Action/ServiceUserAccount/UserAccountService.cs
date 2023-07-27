9eb2cd14f19992c4407050c52e2f8badc061c810
Kareem_DC7X <kareem741ahmed@gmail.com>
2023-07-27 06:51:18 +0300

    User Account Service Improvement

--- Logic_Action/Logic_Action/ServiceUserAccount/UserAccountService.cs
+++ Logic_Action/Logic_Action/ServiceUserAccount/UserAccountService.cs
@@ -1,24 +1,35 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using DBModel.Models;
 using Logic_Action.Encript_Pass;
 using Microsoft.EntityFrameworkCore;
 
 namespace Logic_Action.UserAction
 {
-    public class UserAccount : IDisposable
+    public interface IUserAccountService
+    {
+        Task<Result<bool>> CheckAsync(string searchName);
+        Task<Result<bool>> RegisterAsync(string userName, string password, bool isAdmin);
+        Task<Result<bool>> UpdateUserAsync(string oldName, string newUserName, string newHashPassword, byte[] newSalt, bool newIsAdmin);
+        Task<SignInResult> SignInAsync(string userName, string password);
+        Task<Result<bool>> DeleteUserAsync(string userName);
+    }
+
+    public class UserAccountService : IUserAccountService, IDisposable
     {
         private readonly StoreContext dbContext;
 
-        public UserAccount()
+        public UserAccountService(StoreContext dbContext)
         {
-            dbContext = new StoreContext();
+            this.dbContext = dbContext;
         }
 
-        public Result<bool> Check(string searchName)
+        public async Task<Result<bool>> CheckAsync(string searchName)
         {
             try
             {
-                bool userExists = dbContext.Users.Any(user => user.UserName == searchName);
+                bool userExists = await dbContext.Users.AnyAsync(user => user.UserName == searchName);
                 return Result<bool>.Success(userExists);
             }
             catch (Exception ex)
@@ -27,14 +38,13 @@ namespace Logic_Action.UserAction
             }
         }
 
-        public Result<bool> login(string userName, string password, bool isAdmin)
+        public async Task<Result<bool>> RegisterAsync(string userName, string password, bool isAdmin)
         {
-            var checkResult = Check(userName);
-            
+            var checkResult = await CheckAsync(userName);
 
-            if (checkResult.Data == true)
+            if (checkResult.Data)
             {
-                return Result<bool>.Success(false);
+                return Result<bool>.Success(false); // User already exists
             }
             else if (checkResult.Error != null)
             {
@@ -56,7 +66,7 @@ namespace Logic_Action.UserAction
                 try
                 {
                     dbContext.Users.Add(newUser);
-                    dbContext.SaveChanges();
+                    await dbContext.SaveChangesAsync();
                 }
                 catch (Exception ex)
                 {
@@ -65,69 +75,62 @@ namespace Logic_Action.UserAction
 
                 return Result<bool>.Success(true);
             }
-               
         }
 
-        public Result<bool> UpdateUser(string OldName, string newUserName, string newHashPassword, byte[] newSalt, bool newIsAdmin)
+        public async Task<Result<bool>> UpdateUserAsync(string oldName, string newUserName, string newHashPassword, byte[] newSalt, bool newIsAdmin)
         {
             try
             {
                 // Retrieve the existing user from the database based on the UserName
-                var user = dbContext.Users.FirstOrDefault(u => u.UserName == OldName);
+                var user = await dbContext.Users.FirstOrDefaultAsync(u => u.UserName == oldName);
 
                 if (user == null)
                 {
-
-                    return Result<bool>.Success(false);
+                    return Result<bool>.Success(false); // User not found
                 }
+
                 
-                // Update the user properties with new values
                 user.UserName = newUserName;
                 user.HashPassword = newHashPassword;
                 user.Salt = newSalt;
                 user.IsAdmin = newIsAdmin;
 
                 // Save the changes to the database
-                dbContext.SaveChanges();
+                await dbContext.SaveChangesAsync();
 
                 // Update successful
                 return Result<bool>.Success(true);
             }
             catch (Exception ex)
             {
-
                 return Result<bool>.Failure(ex);
             }
         }
 
-
-
-
-        public SignInResult SignIn(string UserName, string Password)
+        public async Task<SignInResult> SignInAsync(string userName, string password)
         {
             var result = new SignInResult();
 
-            var user = dbContext.Users
-                .Where(u => u.UserName == UserName)
+            var user = await dbContext.Users
+                .Where(u => u.UserName == userName)
                 .Select(u => new { UserName = u.UserName, HashPassword = u.HashPassword, Salt = u.Salt })
-                .FirstOrDefault();
+                .FirstOrDefaultAsync();
 
             if (user == null)
             {
-                
                 result.IsSuccessful = false;
                 result.ErrorMessage = "User not found";
             }
             else
             {
-                if (Encript.VerifyPassword(Password, user.HashPassword, user.Salt))
+                if (Encript.VerifyPassword(password, user.HashPassword, user.Salt))
                 {
                     // Sign-in successful
                     result.IsSuccessful = true;
                 }
                 else
                 {
-                    // Error Password
+                    // Incorrect password
                     result.IsSuccessful = false;
                     result.ErrorMessage = "Incorrect password.";
                 }
@@ -136,14 +139,34 @@ namespace Logic_Action.UserAction
             return result;
         }
 
+        public async Task<Result<bool>> DeleteUserAsync(string userName)
+        {
+            try
+            {
+                var user = await dbContext.Users.FirstOrDefaultAsync(u => u.UserName == userName);
+
+                if (user == null)
+                {
+                    return Result<bool>.Success(false); // User not found
+                }
+
+                dbContext.Users.Remove(user);
+                await dbContext.SaveChangesAsync();
+
+                // Deletion successful
+                return Result<bool>.Success(true);
+            }
+            catch (Exception ex)
+            {
+                return Result<bool>.Failure(ex);
+            }
+        }
 
         public void Dispose()
         {
             dbContext.Dispose();
         }
     }
-
-  
 
     public class Result<T>
     {
